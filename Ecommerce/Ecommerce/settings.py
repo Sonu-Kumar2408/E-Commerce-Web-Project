813e720d46ff582cc8d3d2e8dfdd273b8b22d8fb
Sonu-Kumar2408 <kumarsonuproject@gmail.com>
2024-04-18 19:02:52 +0530

    added all frontend parts

--- Ecommerce/Ecommerce/settings.py
+++ Ecommerce/Ecommerce/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Ecommerceapp',
+    'authcart',
     
 ]
 
@@ -119,7 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+import os
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- Ecommerce/Ecommerce/urls.py
+++ Ecommerce/Ecommerce/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("Ecommerceapp.urls"))
+    path('',include("Ecommerceapp.urls")),
+    path('auth',include("authcart.urls"))
 ]
